2de2608b307021d073ffb0391b10d80dbed9c5e7
liupan <1206284818@qq.com>
2020-10-18 23:26:36 +0800

    vue3 版本支持

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
   },
   resolve: {
     alias: {
-      vue$: 'vue/dist/vue.esm.js',
+      // vue$: 'vue/dist/vue.esm.js',
       '@': resolve('src'),
     },
     extensions: ['*', '.js', '.vue', '.json'],
